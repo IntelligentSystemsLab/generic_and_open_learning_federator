863d975c9a89c435879831b2c6e42119be2e2410
MatZaharia <guozh29@mail2.sysu.edu.cn>
2022-12-03 01:40:27 +0800

    v0.1.0

---
+++ example/test/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author             : GZH
+# @Created Time       : 2022/12/3 1:37
+# @Email              : guozh29@mail2.sysu.edu.cn
+# @Last Modified By   : GZH
+# @Last Modified Time : 2022/12/3 1:37
+from golf_federated.utils.log import loggerhear
+
+if __name__ == '__main__':
+    loggerhear.log("Common Info  ", 'Test Successfully')
