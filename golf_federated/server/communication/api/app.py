93d1c38e7012d9d17dfd9ab0e764749d74f8cb99
MatZaharia <guozh29@mail2.sysu.edu.cn>
2022-12-03 22:41:05 +0800

    v0.1.0

--- golf_federated/server/communication/api/app.py
+++ golf_federated/server/communication/api/app.py
@@ -23,30 +23,60 @@ task_name = 'task1'
 # API for clients to download model.
 @app.route('/download-model', methods=['POST', 'GET'])
 def downloadmodel():
+    """
+
+    API for clients to download model.
+
+    """
+
     return download_model(app.config['SERVER_' + server_name])
 
 
 # API for clients to download task info.
 @app.route('/download-info', methods=['POST', 'GET'])
 def downloadinfo():
+    """
+
+    API for clients to download task info.
+
+    """
+
     return download_info(app.config['SERVER_' + server_name])
 
 
 # API for clients to register.
 @app.route('/client-register', methods=['POST', 'GET'])
 def clientregister():
+    """
+
+    API for clients to register.
+
+    """
+
     return client_register(app.config['SERVER_' + server_name])
 
 
 # API for clients to upload model.
 @app.route('/upload-model', methods=['POST', 'GET'])
 def uploadmodel():
+    """
+
+    API for clients to upload model.
+
+    """
+
     return upload_model(app.config['SERVER_' + server_name])
 
 
 # API for clients to listen to the SSE channel.
 @app.route('/sse')
 def sse():
+    """
+
+    API for clients to listen to the SSE channel.
+
+    """
+
     # TODO: Push messages selectively.
     return Response(
         server_sent_event(
