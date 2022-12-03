51a79167bcf1e12bba1019dcd9d2e082d53ed258
MatZaharia <guozh29@mail2.sysu.edu.cn>
2022-12-03 22:26:33 +0800

    v0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, 'D:\\坚果云\\我的坚果云\\MyGit\\generic-and-open-learning-federator\\golf_federated')
+import os
+import sys
+sys.path.insert(0, 'D:\\坚果云\\我的坚果云\\MyGit\\generic-and-open-learning-federator\\golf_federated')
 
 
 # -- Project information -----------------------------------------------------
