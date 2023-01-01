6d7c9d5d5bd4cb1f035ed6fcaf1a6357ce908917
MatZaharia <guozh29@mail2.sysu.edu.cn>
2023-01-01 20:22:33 +0800

    test(Example): Module for FMNIST

---
+++ example/module/FMNIST/tfmodule.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Author             : GZH
+# @Created Time       : 2023/1/1 20:08
+# @Email              : guozh29@mail2.sysu.edu.cn
+# @Last Modified By   : GZH
+# @Last Modified Time : 2023/1/1 20:08
+
+import tensorflow as tf
+
+
+def create_cnn_for_fmnist():
+    return tf.keras.Sequential(
+        [
+            tf.keras.layers.Conv2D(
+                32, kernel_size=(3, 3),
+                activation='relu',
+                input_shape=(28, 28, 1),
+                padding='SAME',
+            ),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides=2),
+            tf.keras.layers.Conv2D(
+                64, kernel_size=(3, 3),
+                activation='relu',
+                padding='SAME',
+            ),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides=2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.25),
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dropout(0.3),
+            tf.keras.layers.Dense(10, activation='softmax'),
+        ]
+    )
+
+model = 'create_cnn_for_mnist'
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.005)
+loss = tf.keras.losses.CategoricalCrossentropy()
+batch_size = 128
+train_epoch = 1
+library = 'tensorflow'
+metrics = ["accuracy"]
