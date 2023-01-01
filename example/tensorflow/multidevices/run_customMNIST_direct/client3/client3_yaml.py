a2b46a55bcc4dffd28326f6d3de2f00408744dd9
MatZaharia <guozh29@mail2.sysu.edu.cn>
2023-01-01 20:22:05 +0800

    test(Example): Add a synchronous and multi-device example for MNIST by YAML

---
+++ example/tensorflow/multidevices/run_customMNIST_direct/client3/client3_yaml.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author             : GZH
+# @Created Time       : 2023/1/1 18:17
+# @Email              : guozh29@mail2.sysu.edu.cn
+# @Last Modified By   : GZH
+# @Last Modified Time : 2023/1/1 18:17
+
+import warnings
+
+warnings.filterwarnings("ignore", category=Warning)
+
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+from golf_federated.utils.config import load_yaml_config
+
+if __name__ == '__main__':
+
+    load_yaml_config('./client3.yaml')
