bf76f998b1e2d2bf52376d6a3aacbfb12896604f
MatZaharia <guozh29@mail2.sysu.edu.cn>
2024-06-08 20:02:02 +0800

    Updating Documents

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
     # 'sphinx.ext.napoleon',
     # 'sphinx.ext.mathjax',
     # 'recommonmark',
-    'sphinx_markdown_tables',
+    # 'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
